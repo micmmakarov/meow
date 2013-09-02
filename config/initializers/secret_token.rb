236bdc2f7681dada8975e2304f83b9fe5d360867
Michael Makarov <info1117@gmail.com>
2013-09-01 19:24:46 -0700

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Meow::Application.config.secret_key_base = 'a67cc4ecf94c227e9e0171d83a5ca8c161a7c05332e1e9601797ec3f0c3953d2ce4de4997759a008202bea6062c5e86c829d15b80897cc931677a40d2ab97369'
